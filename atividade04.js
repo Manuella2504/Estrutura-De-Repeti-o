8d75b65016c9093184ce0981839e212840bc6345
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-07-28 10:31:27 -0300

    Update atividade04.js

--- atividade04.js
+++ atividade04.js
@@ -1,2 +1,13 @@
-/*Crie um objeto com informações de um produto (nome, preço, quantidade) e
- use for...in para imprimir todas as suas propriedades. */
+/*Crie um objeto com informações de um produto (nome, preço, quantidade) e use for...in para imprimir todas as suas propriedades. */
+
+const produto = {
+    Nome : "Escova de cabelo",
+    preco: 15.00,
+    quantidade: 3
+};
+
+// O objeto ``e algo criado que armazena valores e chaves, funciona como uma caixa de informações cadastradas.
+
+for (const especificacoes in produto){
+    console.log(`${propriedade}: ${produto[propriedade]}`);
+}
