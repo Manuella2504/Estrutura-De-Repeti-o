f09ec057a7abd81e65590ddd686e7f74ff515409
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-07-28 09:37:09 -0300

    Atividade 3 criada

--- atividade03.js
+++ atividade03.js
@@ -1,1 +1,7 @@
 /*Crie um array de nomes e use for...of para imprimir cada nome com uma saudação personalizada.*/
+const nomes = [Manuella, Ramon, Marcelle, Camila];
+
+for (const nome of nomes){
+//A constante é criada dentro do for, determinando que nome servirá para os nomes criados no array(lista)
+    console.log("Nome impresso:" + nome);
+}
